a03d45c3995f2aedcb5ab043b15af0896764a0a8
cookie0verload <matt@robey.io>
2015-04-21 21:26:08 +0100

    Initial commit.

---
+++ binday.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+bindayurl=""
+tmpfile='/tmp/localdata.tmp'
+tomorrow=`date +"%-d%-m%Y" -d "tomorrow"`
+tomorrow_nice=`date +"%d/%m/%Y" -d "tomorrow"`
+
+echo "$(date +"%m-%d-%Y-%T"): Starting bin run"
+
+#Pull down local news into tmp
+wget -q -O $tmpfile "$bindayurl"
+
+#Check contents of file, if not valid then error
+
+if [ -e $tmpfile ]
+then
+  hasdata=`grep -E '(Household waste bin|Mixed recycling box|Paper recycling bin|Garden waste recycling bin)' $tmpfile`
+  if [ -z "$hasdata" ]
+  then
+    echo "Cant find data yo."
+    exit
+  fi
+else
+  echo "no file bro"
+  exit
+fi
+
+#scrape and store next bin collection dates
+
+#Household waste bin
+housenext=`grep "Household waste bin" $tmpfile | awk -F "next collection will be on" '{ print $2 }'| awk  -F "." '{ print $1 }' | sed -e 's/\///g' -e 's/ //g'`
+#Mixed recycling box
+mixednext=`grep "Mixed recycling box" $tmpfile | awk -F "next collection will be on" '{ print $2 }'| awk  -F "." '{ print $1 }' | sed -e 's/\///g' -e 's/ //g'`
+#Paper recycling bin
+papernext=`grep "Paper recycling bin" $tmpfile | awk -F "next collection will be on" '{ print $2 }'| awk  -F "." '{ print $1 }' | sed -e 's/\///g' -e 's/ //g'`
+#Garden waste recycling bin
+gardennext=`grep "Garden waste recycling bin" $tmpfile | awk -F "next collection will be on" '{ print $2 }'| awk  -F "." '{ print $1 }' | sed -e 's/\///g' -e 's/ //g'`
+
+#Compare dates and send notification if the bin needs putting out tonight.
+output="Tomorrows Date: $tomorrow_nice\n\n"
+echo "---Data---"
+echo "Tomorrows Date: $tomorrow"
+echo "Household Collection: $housenext"
+echo "Mixed Recycle Collection: $mixednext"
+echo "Paper Collection: $papernext"
+echo "Garden Collection: $gardennext"
+echo "---END DATA---"
+
+#Household waste bin
+if [ -n "$housenext" ]
+then
+  if [ "$tomorrow" -eq "$housenext" ]
+  then
+    output="$output Put the fucking household waste bin out. (Big Green One)\n\n"
+    sendnotif=1
+ #   echo "Put the fucking household waste bin out. (Big Green One)\n"
+  fi
+fi
+
+#Mixed recycling box
+if [ -n "$mixednext" ]
+then
+  if [ "$tomorrow" -eq "$mixednext" ]
+  then
+    output="$output Put the fucking mixed recycling box out. (Little green one - That we don't have)\n\n"
+    sendnotif=1
+  fi
+fi
+
+#Paper recycling bin
+if [ -n "$papernext" ]
+then
+  if [ "$tomorrow" -eq "$papernext" ]
+  then
+    output="$output Put the fucking paper recycling bin out. (Big brown one without holes.)\n\n"
+    sendnotif=1
+  fi
+fi
+
+#Garden waste recycling bin
+if [ -n "$gardennext" ]
+then
+  if [ "$tomorrow" -eq "$gardennext" ]
+  then
+    output="$output Put the fucking Garden waste recycling bin out. (Big brown one with holes.)\n\n"
+    sendnotif=1
+  fi
+fi
+
+#Time to push
+
+if [[ $sendnotif -eq 1 ]]
+then
+  echo -e $output
+  echo "Sending Notification"
+  curl -s \
+    --form-string "token=" \
+    --form-string "user=" \
+    --form-string "message=$(echo -e $output)" \
+    https://api.pushover.net/1/messages.json
+fi
+
+echo "$(date +"%m-%d-%Y-%T"): Ending bin run"
+
+rm -f $tmpfile
